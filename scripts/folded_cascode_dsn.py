05af783f15b47a581d6f06050aeac80f6d77b861
LIN <zhenghan_lin@hpse-12.EECS.Berkeley.EDU>
2019-05-20 22:16:49 -0700

    May 20 fixed

--- scripts/folded_cascode_dsn.py
+++ scripts/folded_cascode_dsn.py
@@ -22,13 +22,9 @@ def simulate(prj, temp_lib, impl_lib, tb_name, cell_name, sim_params):
 
     # configure tb
     tb = prj.configure_testbench(tb_lib=impl_lib, tb_cell=tb_name)
-<<<<<<< HEAD
-    tb.set_parameter('vdd', sim_params[0])
-    tb.set_parameter('Rsense', sim_params[1])
-    tb.add_output('imag', """imag(VF("/voutp") - VF("/voutn"))""")
-=======
+
     tb.set_parameter('Rsense',sim_params['Rsense'])
->>>>>>> zhenghan
+
     tb.update_testbench()
 
     # rum simulation
@@ -42,13 +38,6 @@ def simulate(prj, temp_lib, impl_lib, tb_name, cell_name, sim_params):
     print("Hi2")
 
     # Get results
-<<<<<<< HEAD
-    gain = results['gain']
-    phase = results['phase']
-    freq = results['freq']
-    real = results['imag']
-    return gain, phase, real, freq
-=======
     results = load_sim_results(tb.save_dir)
 
     #print(results)
@@ -60,7 +49,6 @@ def simulate(prj, temp_lib, impl_lib, tb_name, cell_name, sim_params):
     phase = results['amp_phase']
     freq = results['freq']
     return gain, phase, freq
->>>>>>> zhenghan
 
 
 def design(prj, temp_lib, impl_lib, tb_name, cell_name, sch_params):
@@ -68,21 +56,7 @@ def design(prj, temp_lib, impl_lib, tb_name, cell_name, sch_params):
     print("Simulating ...")
 
     generate(prj, temp_lib, impl_lib, cell_name, sch_params)
-<<<<<<< HEAD
 
-    gain, phase, real, freq = simulate(prj, temp_lib, impl_lib, tb_name, cell_name, sim_params=[1.2, 5e3])
-
-    plt.figure()
-    plt.subplot(3,1,1)
-    plt.semilogx(freq,gain,'r')
-
-    plt.subplot(3,1,2)
-    plt.semilogx(freq,phase,'y')
-
-    plt.subplot(3,1,3)
-    plt.semilogx(freq,real,'b')
-
-=======
     sim_params = {'Rsense': 5000}
 
     gain, phase, freq = simulate(prj, temp_lib, impl_lib, tb_name, cell_name, sim_params, show_plot=False)
@@ -92,7 +66,7 @@ def design(prj, temp_lib, impl_lib, tb_name, cell_name, sch_params):
     plt.semilogx(freq,gain)
     plt.subplot(2,1,2)
     plt.semilogx(freq,gain)
->>>>>>> zhenghan
+
     plt.show(block=False)
 
 
